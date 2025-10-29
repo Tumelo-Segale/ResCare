bea724df24fa170e3edff8bb07cfc1462795067d
TUJAR <149502014+Tumelo-Segale@users.noreply.github.com>
2025-10-29 04:22:42 +0200

    Update LandingPage.jsx

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../assets/Logo.png";
+import Logo from "./assets/Logo.png";
 import "./LandingPage.css";
 import { useNavigate } from "react-router-dom";
 
